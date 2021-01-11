63f8a89025fe3a408c859080a51cfb7455cfdc49
Luke Matheson <38731081+cardinal7987@users.noreply.github.com>
2021-01-10 23:07:54 -0500

    initial commit of console application, prints output of a simple SQL command

---
+++ PracticeProjects/Project1/Project1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection("Server=.\\SQLEXPRESS;Database=NFLTeams;Integrated Security=true");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM TeamInformation", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("Team Name: {0}", reader.GetString(1));
+            }
+
+            reader.Close();
+            conn.Close();
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
